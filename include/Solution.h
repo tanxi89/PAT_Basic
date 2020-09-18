3708221537e453a1f630cc04eaea0fa2dfc8f3a6
tanxi89 <darkwings98@163.com>
2020-09-18 19:41:34 +0800

    first commit

---
+++ include/Solution.h
@@ -0,0 +1,13 @@
+//
+// Created by Administrator on 2020/9/18 0018.
+//
+
+#ifndef PAT_BASIC_SOLUTION_H
+#define PAT_BASIC_SOLUTION_H
+
+class Solution {
+public:
+    
+};
+
+#endif //PAT_BASIC_SOLUTION_H
